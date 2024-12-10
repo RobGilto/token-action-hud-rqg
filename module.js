508cb5f5d4c44da32eb2fe8366c45aa456ad2236
GiltoGames <141191342+RobGilto@users.noreply.github.com>
2024-12-10 12:30:52 +1100

    Create module.js

---
+++ module.js
@@ -0,0 +1,36 @@
+{
+    "id": "token-action-hud-rqg",
+    "title": "Token Action HUD - RQG",
+    "description": "Integration of Token Action HUD with the RQG system.",
+    "version": "1.0.0",
+    "minimumCoreVersion": "10",
+    "compatibleCoreVersion": "12",
+    "authors": [
+      {
+        "name": "Your Name",
+        "email": "robgiltol@gmail.com",
+        "url": "https://github.com/RobGilto/token-action-hud-rqg"
+      }
+    ],
+    "url": "https://github.com/RobGilto/token-action-hud-rqg",
+    "manifest": "https://your-raw-url-for-module.json",
+    "download": "https://your-download-url-for-module.zip",
+    "type": "module",
+    "esmodules": [
+      "scripts/module.js"
+    ],
+    "requires": [
+      {
+        "id": "token-action-hud-core",
+        "type": "module"
+      }
+    ],
+    "dependencies": [
+      {
+        "id": "rqg",
+        "type": "system"
+      }
+    ],
+    "socket": false
+  }
+  
